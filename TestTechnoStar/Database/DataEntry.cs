13868c6bd0d1f6b738baac279775b69909d6c5e6
Alexander Bocharov <alexbocharov2@gmail.com>
2020-07-01 19:22:11 +0300

    Added database classes

---
+++ TestTechnoStar/Database/DataEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTechnoStar.Database
+{
+    /// <summary>
+    /// Текстовые данные
+    /// </summary>
+    public class DataEntry
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ TestTechnoStar/Database/LogEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TestTechnoStar.Database
+{
+    public class LogEntry
+    {
+        public DateTime CreatedDate { get; set; }
+        public virtual DataEntry Data { get; set; }
+    }
+}
